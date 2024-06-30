cd0ac36e4a6a57e1ea5d7e8a8a60d1e66cb1f1ca
sasp1983 <saspsasp@hotmail.com>
2024-06-30 17:15:05 +0200

    a

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-let barTop = document.querySelector(".bar-top");
+let barTop = document.querySelector(".bar-top")
 let barBottom = document.querySelector(".bar-bottom");
 let button = document.querySelector(".button");
 let imgWrapper = document.querySelector(".img-wrapper");
-let up = document.querySelector(".up");
-
-// let img = document.createElement('img');
-// img.className = 'pic';
 
 function barsOpenToggle() {
   barTop.classList.add("up");
   barBottom.classList.add("down");
-
 }
 
 function barsClosedToggle() {
@@ -22,50 +17,74 @@ button.addEventListener("click", function () {
   fetchRandomImage();
 });
 
-async function fetchRandomImage() {
-  const url = `https://picsum.photos/4000/4000?sig=${Math.random()}`;
+// async function fetchRandomImage() {
+//   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ`;
 
-  const img = document.createElement("img");
+//   const img = document.createElement("img");
 
+//   if (
+//     !barTop.classList.contains("up") &&
+//     !barBottom.classList.contains("down")
+//   ) {
+//     try {
+//       const response = await fetch(url);
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       const blob = await response.blob();
+//       const imageUrl = URL.createObjectURL(blob);
+
+      
+
+//       // Create an img element and set its source to the imageUrl
+
+//       img.src = imageUrl;
+//       img.alt = "Random Image";
+//       img.style.width = "100%";
+//       img.className = "image";
+//       imgWrapper.appendChild(img);
+//       img.onload = barsOpenToggle();
+//     } catch (error) {
+//       console.error("Error fetching the image:", error);
+//     }
+//   } else {
+//     barsClosedToggle();
+//     fetchRandomImage();
+//   }
+
+//   // Append the image to the body or any other container
+//   // document.body.appendChild(img);
+
+//   if (imgWrapper.childElementCount >= 2) {
+//     imgWrapper.firstChild.remove();
+//   }
+// }
+
+const fetchRandomImage = async () => {
   if (
     !barTop.classList.contains("up") &&
     !barBottom.classList.contains("down")
-) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const blob = await response.blob();
-      const imageUrl = URL.createObjectURL(blob);
+  ) {
+  try {
+  const config = { headers: { Accept: "application/json" } };
+  const response = await axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ", config);
+  const data = response.data;
+  console.log(response);
+  console.dir(data.photos);
 
-      // Create an img element and set its source to the imageUrl
-
-      img.src = imageUrl;
-      img.alt = "Random Image";
-      img.style.width = "100%";
-      img.className = "image";
-      imgWrapper.appendChild(img);
-
-      
-      img.onload = setTimeout(barsOpenToggle, "1s");
-      
-    } catch (error) {
-      console.error("Error fetching the image:", error);
-    }
-  } else {
-    barsClosedToggle();
-    fetchRandomImage();
-    // imgWrapper.removeChild(".image");
-    // imgWrapper.replaceChild(img);
+  const img = document.createElement('img');
+  imgWrapper.append(img);
+   img.src = data.photos[Math.floor(Math.random() * data.photos.length)].img_src;
+   img.onload = barsOpenToggle();
+   console.log(img)
+  } catch (error) {
+    console.error("ERROR", error);
   }
-
-  // Append the image to the body or any other container
-  // document.body.appendChild(img);
-
-  if (imgWrapper.childElementCount >= 2) {
-    imgWrapper.firstChild.remove();
-  }
+} else {
+  barsClosedToggle();
+  fetchRandomImage();
 }
-
-// Call the function to fetch and display the random image
+if (imgWrapper.childElementCount >= 2) {
+  imgWrapper.firstChild.remove();
+}
+}
