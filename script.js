f8006600b2ab0e6199f146138d4bdc8cb0ebd8cb
sasp1983 <saspsasp@hotmail.com>
2024-07-01 15:03:52 +0200

    a

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //declare element variables
-
 let barTop = document.querySelector(".bar-top")
 let barBottom = document.querySelector(".bar-bottom");
 let button = document.querySelector(".button");
@@ -23,6 +22,81 @@ function barsClosedToggle() {
 button.addEventListener("click", function () {
   fetchRandomImage();
 });
+
+
+//main function
+const fetchRandomImage = async () => {
+  
+  //show loading icon while image loads behind bars
+  function showLoader () {
+  loadingImage.classList.add("show-loader");
+  }
+  setTimeout(showLoader,300)
+
+  if (
+    !barTop.classList.contains("up") &&
+    !barBottom.classList.contains("down")
+  ) {
+  try {
+  const config = { headers: { Accept: "application/json" } };
+  const response = await axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ", config);
+  const data = response.data;
+  console.log(response.data);
+
+  const img = document.createElement('img');
+  imgWrapper.append(img);
+
+    //randomize image link from json fetch
+    img.src = data.photos[Math.floor(Math.random() * data.photos.length)].img_src;
+
+    img.onload = setTimeout(barsOpenToggle, 300);
+
+
+    //hide loading icon when image is loaded
+    img.onload = 
+    setTimeout (function () {
+      loadingImage.classList.remove('show-loader');
+    },450);
+
+    //create download link to save image
+    downloadLink.href = img.src;
+    function showLink () {
+      downloadLink.classList.add('show-link');
+    }
+    showLink()
+
+   console.log(img)
+  } catch (error) {
+    console.error("ERROR", error);
+  }
+} else {
+  //close sliding bars and execute main function again
+  barsClosedToggle();
+  fetchRandomImage();
+}
+
+//delete old images
+if (imgWrapper.childElementCount >= 2) {
+  imgWrapper.firstChild.remove();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=======================//
+// old chat GPT fetch function
 
 // async function fetchRandomImage() {
 //   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ`;
@@ -64,71 +138,3 @@ button.addEventListener("click", function () {
 //     imgWrapper.firstChild.remove();
 //   }
 // }
-
-
-
-
-
-
-
-//main function
-const fetchRandomImage = async () => {
-  
-  function showLoader () {
-  loadingImage.classList.add("show-loader");
-  }
-  setTimeout(showLoader,300)
-
-  if (
-    !barTop.classList.contains("up") &&
-    !barBottom.classList.contains("down")
-  ) {
-  try {
-  const config = { headers: { Accept: "application/json" } };
-  const response = await axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ", config);
-  const data = response.data;
-  console.log(response.data);
-  // console.dir(data.photos);
-
-  const img = document.createElement('img');
-  imgWrapper.append(img);
-
-  // function showImageOnLoad() {
-  //   if (img.loadingImage = '100%') {
-  //     barsOpenToggle();
-  //   }
-  // }
-
-  img.onload = setTimeout(barsOpenToggle, 300);
-
-  //show loader icon while image is loading behind closed bars
-    img.src = data.photos[Math.floor(Math.random() * data.photos.length)].img_src;
-    img.onload = 
-    setTimeout (function () {
-      loadingImage.classList.remove('show-loader');
-    },450);
-
-    //create download link to save image
-    downloadLink.href = img.src;
-    function showLink () {
-      downloadLink.classList.add('show-link');
-    }
-    showLink()
-
-   console.log(img)
-  } catch (error) {
-    console.error("ERROR", error);
-  }
-} else {
-  //close sliding bars and execute main function again
-  barsClosedToggle();
-  fetchRandomImage();
-}
-
-//delete old images
-if (imgWrapper.childElementCount >= 2) {
-  imgWrapper.firstChild.remove();
-}
-}
-
-
